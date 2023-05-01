0431a1af6d47f8d3a1135b5bab8e2377080959ee
Ankush Madharha <ankush@Ankushs-MacBook-Pro.local>
2023-05-01 18:06:58 -0400

    slight change to the molecule.i file, remake

--- C-molecule-library/molecule_wrap.c
+++ C-molecule-library/molecule_wrap.c
@@ -3439,8 +3439,8 @@ SWIGINTERN void molecule_append_atom(struct molecule *self,char element[3],doubl
     }
 SWIGINTERN void molecule_append_bond(struct molecule *self,unsigned short a1,unsigned short a2,unsigned char epairs){
         bond b1;
-        b1.a1 = a1 - 1;
-        b1.a2 = a2 - 1;
+        b1.a1 = a1;
+        b1.a2 = a2;
         b1.atoms = self->atoms;
         b1.epairs = epairs;
         compute_coords(&b1);
