e64c6bb946b66311a5e57853b052fbd51849005b
Ankush Madharha <ankush@Ankushs-MacBook-Pro.local>
2023-05-01 19:23:39 -0400

    svg generation and database implemented

---
+++ python/mol_display.py
@@ -0,0 +1,138 @@
+from molecule import molecule
+
+# CONSTANTS
+RADIUS = None
+ELEMENT_NAME = None
+
+HEADER = """<svg version="1.1" width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">"""
+ 
+FOOTER = """</svg>"""
+
+OFFSET_X = 500 
+OFFSET_Y = 500
+
+class Atom:
+    def __init__(self, c_atom: atom):
+        """
+        Initializes a new Atom object with the given atomic number.
+
+        Args:
+            c_atom (atom): An atom from the molecule module with attributes 
+                           element (str), x (double), y (double), and z (double).
+        """
+        self.atom = c_atom
+        self.z = c_atom.z
+
+    def svg(self) -> str:
+        """
+        Returns an SVG circle element representing this Atom object.
+
+        The position, radius, and fill color of the circle are determined by the
+        attributes of the underlying Atom object.
+
+        Returns:
+            str: An SVG circle element as a string.
+        """
+        x = (self.atom.x * 100.0) + OFFSET_X
+        y = (self.atom.y * 100.0) + OFFSET_Y
+        radi = RADIUS[self.atom.element]
+        color = ELEMENT_NAME[self.atom.element]
+
+        return '  <circle cx="%.2f" cy="%.2f" r="%d" fill="url(#%s)"/>\n' % (x, y, radi, color)
+    
+class Bond:
+    def __init__(self, c_bond: bond):
+        """
+        Initializes a new Bond object with the given bond type.
+
+        Args:
+            c_bond (bone): A bond from the molecule module with attributes
+                           a1, a2, epairs, atoms, x1, x2, y1, y2, z, len, dx, and dy
+        """
+        self.bond = c_bond
+        self.z = c_bond.z
+
+    def svg(self) -> str:  
+        """
+        Returns an SVG polygon element representing this Bond object.
+
+        The position and shape of the polygon are determined by the attributes of the
+        underlying Bond object.
+
+        Returns:
+            str: An SVG polygon element as a string.
+        """
+        x1 = ((self.bond.x1 * 100) + OFFSET_X) - (self.bond.dy * 10)
+        y1 = ((self.bond.y1 * 100) + OFFSET_Y) + (self.bond.dx * 10)
+        
+        x2 = ((self.bond.x1 * 100) + OFFSET_X) + (self.bond.dy * 10)
+        y2 = ((self.bond.y1 * 100) + OFFSET_Y) - (self.bond.dx * 10)
+
+        x3 = ((self.bond.x2 * 100) + OFFSET_X) + (self.bond.dy * 10)
+        y3 = ((self.bond.y2 * 100) + OFFSET_Y) - (self.bond.dx * 10)
+
+        x4 = ((self.bond.x2 * 100) + OFFSET_X) - (self.bond.dy * 10)
+        y4 = ((self.bond.y2 * 100) + OFFSET_Y) + (self.bond.dx * 10)
+
+        return '  <polygon points="%.2f,%.2f %.2f,%.2f %.2f,%.2f %.2f,%.2f" fill="green"/>\n' % (x1, y1, x2, y2, x3, y3, x4, y4)
+
+class Molecule(molecule):
+    def svg(self) -> str:
+        """
+        Returns an SVG string representing this Molecule object.
+
+        The SVG string is generated from the underlying Atom and Bond objects in the
+        Molecule, sorted by atomic number and bond type, respectively.
+
+        Returns:
+            str: An SVG string representing this Molecule object.
+        """
+        atoms = []
+        for i in range(self.atom_no):
+            atoms.append(Atom(self.get_atom(i)))
+        bonds = []
+        for i in range(self.bond_no):
+            bonds.append(Bond(self.get_bond(i)))
+
+        atoms.sort(key=lambda atom: atom.z)
+        bonds.sort(key=lambda bond: bond.z)
+
+        svg_strings = []
+        while atoms and bonds:
+            a1 = atoms[0]
+            b1 = bonds[0] 
+            if a1.z < b1.z:
+                svg_strings.append(a1.svg())
+                atoms.pop(0)
+            else:
+                svg_strings.append(b1.svg())
+                bonds.pop(0)
+        svg_strings += [atom.svg() for atom in atoms] + [bond.svg() for bond in bonds]
+
+        return HEADER + "".join(svg_strings) + FOOTER
+    
+    def parse(self, file: IO):
+        """
+        Parses a sdf data file and populates this Molecule object with Atom and Bond objects.
+
+        Args:
+            file (IO): An IO object representing the molecule data file.
+        """
+        atom_count, bond_count = 0, 0
+
+        for i in range(3):
+            file.readline()
+
+        line = file.readline().strip()
+        atom_count = int(line.split()[0])
+        bond_count = int(line.split()[1])
+
+        for i in range(atom_count):
+            line = file.readline().strip()
+            atom_info = line.split()
+            self.append_atom(atom_info[3], float(atom_info[0]), float(atom_info[1]), float(atom_info[2]))
+
+        for i in range(bond_count):
+            line = file.readline().strip()
+            bond_info = line.split()
+            self.append_bond(int(bond_info[0])-1, int(bond_info[1])-1, int(bond_info[2]))
---
+++ python/molsql.py
@@ -0,0 +1,223 @@
+import os
+import sqlite3
+import mol_display
+
+class Database:
+    def __init__(self, reset: bool=False):
+        """
+        Initializes a new Database object and connects to an SQLite database.
+
+        Args:
+            reset (bool, optional): If True, deletes the existing database file and creates
+                a new one. Defaults to False.
+        """
+        self.conn = sqlite3.connect('../molecules.db')
+        if reset:
+            self.conn.close()
+            os.remove('../molecules.db')
+            self.conn = sqlite3.connect('../molecules.db')
+
+    def create_tables(self):
+        """
+        Creates the necessary tables in the SQLite database for storing Elements, Atoms, Bonds, Molecules,
+        and their relationships.
+        """
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS Elements
+                        (ELEMENT_NO   INTEGER,
+                        ELEMENT_CODE VARCHAR(3) PRIMARY KEY,
+                        ELEMENT_NAME VARCHAR(32),
+                        COLOUR1      CHAR(6),
+                        COLOUR2      CHAR(6),
+                        COLOUR3      CHAR(6),
+                        RADIUS       DECIMAL(3));""")  
+        
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS Atoms
+                        (ATOM_ID      INTEGER PRIMARY KEY AUTOINCREMENT,
+                        ELEMENT_CODE VARCHAR(3) REFERENCES Elements(ELEMENT_CODE),
+                        X            DECIMAL(7,4),
+                        Y            DECIMAL(7,4),
+                        Z            DECIMAL(7,4));""")  
+        
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS Bonds
+                        (BOND_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        A1      INTEGER,
+                        A2      INTEGER,
+                        EPAIRS  INTEGER);""")  
+
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS Molecules
+                        (MOLECULE_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        NAME        TEXT UNIQUE);""") 
+
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS MoleculeAtom
+                        (MOLECULE_ID INTEGER,
+                        ATOM_ID     INTEGER,
+                        PRIMARY KEY (MOLECULE_ID, ATOM_ID),
+                        FOREIGN KEY (MOLECULE_ID) REFERENCES Molecules(MOLECULE_ID),
+                        FOREIGN KEY (ATOM_ID) REFERENCES Atoms(Atom_ID));""") 
+
+        self.conn.execute("""CREATE TABLE IF NOT EXISTS MoleculeBond
+                        (MOLECULE_ID INTEGER,
+                        BOND_ID     INTEGER,
+                        PRIMARY KEY (MOLECULE_ID, BOND_ID),
+                        FOREIGN KEY (MOLECULE_ID) REFERENCES Molecules(MOLECULE_ID),
+                        FOREIGN KEY (BOND_ID) REFERENCES Bonds(BOND_ID));""")
+        
+    def __setitem__(self, table: str, values: tuple):
+        """
+        Inserts a new row into the specified table in the SQLite database with the given values.
+
+        Args:
+            table (str): The name of the table to insert the row into.
+            values (tuple): A tuple of values to insert into the row.
+        """
+        placeholders = ','.join(['?']*len(values))
+        query = f"INSERT INTO {table} VALUES ({placeholders})"
+        self.conn.execute(query, values)
+        self.conn.commit()
+
+    def add_atom(self, molname: str, atom: mol_display.Atom):
+        """
+        Adds a new Atom object to the specified molecule in the database.
+
+        Args:
+            molname (str): The name of the molecule to add the atom to.
+            atom (Atom): An Atom object representing the atom to add.
+        """
+        element_code = atom.atom.element
+        x = atom.atom.x
+        y = atom.atom.y
+        z = atom.atom.z
+
+        self['Atoms'] = (None, element_code, x, y, z)
+        
+        result = self.conn.execute("SELECT MOLECULE_ID FROM Molecules WHERE NAME = ?", (molname,))
+        row = result.fetchone()
+        if row is None:
+            return
+        
+        mol_id = row[0]
+        atom_id = self.conn.execute("SELECT last_insert_rowid()").fetchone()[0]
+        self['MoleculeAtom'] = (mol_id, atom_id)
+
+    def add_bond(self, molname: str, bond: mol_display.Bond):
+        """
+        Adds a new Bond object to the specified molecule in the database.
+
+        Args:
+            molname (str): The name of the molecule to add the bond to.
+            bond (Bond): A Bond object representing the bond to add.
+        """
+        a1 = bond.bond.a1
+        a2 = bond.bond.a2
+        epairs = bond.bond.epairs
+
+        self['Bonds'] = (None, a1, a2, epairs)
+
+        result = self.conn.execute("SELECT MOLECULE_ID FROM Molecules WHERE NAME = ?", (molname,))
+        row = result.fetchone()
+        if row is None:
+            return
+        
+        mol_id = row[0]
+        bond_id = self.conn.execute("SELECT last_insert_rowid()").fetchone()[0]
+        self['MoleculeBond'] = (mol_id, bond_id)
+
+    def add_molecule(self, name: str, fp: IO):
+        """
+        Adds a new molecule to the database with the given name and file pointer.
+
+        Args:
+            name (str): The name of the molecule to add.
+            fp (IO): A file pointer to the molecule data file.
+        """
+        molecule = mol_display.Molecule()
+        molecule.parse(fp)
+
+        self['Molecules'] = (None, name)
+
+        for i in range(molecule.atom_no):
+            atom = mol_display.Atom(molecule.get_atom(i))
+            self.add_atom(name, atom)
+
+        for i in range(molecule.bond_no):
+            bond = mol_display.Bond(molecule.get_bond(i))
+            self.add_bond(name, bond)
+
+    def load_mol(self, name) -> mol_display.Molecule:
+        """
+        Retrieves a molecule from the database with the given name, and returns a corresponding
+        mol_display.Molecule object.
+
+        Args:
+            name (str): The name of the molecule to retrieve.
+
+        Returns:
+            mol_display.Molecule: A mol_display.Molecule object representing the specified molecule.
+        """
+        query = f"""SELECT Atoms.*, Elements.*
+                    FROM Atoms
+                    JOIN Elements ON Atoms.ELEMENT_CODE = Elements.ELEMENT_CODE
+                    JOIN MoleculeAtom ON Atoms.ATOM_ID = MoleculeAtom.ATOM_ID
+                    JOIN Molecules ON Molecules.MOLECULE_ID = MoleculeAtom.MOLECULE_ID
+                    WHERE Molecules.NAME = '{name}'
+                    ORDER BY Atoms.ATOM_ID ASC"""
+        atoms_result = self.conn.execute(query).fetchall()
+    
+        query = f"""SELECT Bonds.*
+            FROM Bonds
+            JOIN MoleculeBond ON Bonds.BOND_ID = MoleculeBond.BOND_ID
+            JOIN Molecules ON Molecules.MOLECULE_ID = MoleculeBond.MOLECULE_ID
+            WHERE Molecules.NAME = '{name}'
+            ORDER BY Bonds.BOND_ID ASC"""
+        bonds_result = self.conn.execute(query).fetchall()
+
+        mol = mol_display.Molecule()
+        for atom in atoms_result:
+            mol.append_atom(atom[1], atom[2], atom[3], atom[4])
+        for bond in bonds_result:
+            mol.append_bond(bond[1], bond[2], bond[3])
+
+        return mol
+
+    def radius(self) -> dict[str, str]:
+        """
+        Returns a dictionary mapping element codes to their corresponding atomic radii.
+
+        Returns:
+            dict: A dictionary mapping element codes to their corresponding atomic radii.
+        """
+        query = "SELECT ELEMENT_CODE, RADIUS FROM Elements"
+        results = self.conn.execute(query).fetchall()
+        return {element_code: radius for element_code, radius in results}
+    
+    def element_name(self) -> dict[str, str]:
+        """
+        Returns a dictionary mapping element codes to their corresponding element names.
+
+        Returns:
+            dict: A dictionary mapping element codes to their corresponding element names.
+        """
+        cursor = self.conn.execute("SELECT ELEMENT_CODE, ELEMENT_NAME FROM Elements")
+        element_dict = {row[0]: row[1] for row in cursor}
+        return element_dict
+
+    def radial_gradients(self) -> str:
+        """
+        Returns a string containing the XML definitions for radial gradients based on element colors.
+
+        Returns:
+            str: A string containing the XML definitions for radial gradients based on element colors.
+        """
+        gradients = ""
+        elements = self.conn.execute("SELECT ELEMENT_NAME, COLOUR1, COLOUR2, COLOUR3 FROM Elements")
+
+        for row in elements:
+            gradient = f"""
+            <radialGradient id="{row[0]}" cx="-50%" cy="-50%" r="220%" fx="20%" fy="20%">
+                <stop offset="0%" stop-color="#{row[1]}"/>
+                <stop offset="50%" stop-color="#{row[2]}"/>
+                <stop offset="100%" stop-color="#{row[3]}"/>
+            </radialGradient>"""
+            gradients += gradient
+
+        return gradients
