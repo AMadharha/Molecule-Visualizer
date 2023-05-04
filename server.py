1fd1faeb7f0c28806e851def62f58cedb3b31694
Ankush Madharha <ankush@Ankushs-MacBook-Pro.local>
2023-05-04 17:13:51 -0400

    flask conversion

--- server.py
+++ server.py
@@ -1,124 +1,89 @@
-import http.server
-import socketserver
+from flask import Flask, request, send_from_directory, jsonify, abort
 import os
-import mimetypes
 from molsql import Database
-import json
-import cgi
 import mol_display
 
-class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        file_path = 'index.html' if self.path == '/' else self.path[1:]
-        file_path = os.path.join(os.path.dirname(__file__), file_path)
+app = Flask(__name__)
 
-        if os.path.isfile(file_path):
-            mime_type, _ = mimetypes.guess_type(file_path)
+@app.route('/', methods=['GET'])
+def index():
+    return send_from_directory(os.path.dirname(__file__), 'index.html')
 
-            self.send_response(200)
-            self.send_header("Content-type", mime_type)
-            self.end_headers()
+@app.route('/<path:path>', methods=['GET'])
+def static_files(path):
+    file_path = os.path.join(os.path.dirname(__file__), path)
+    if os.path.isfile(file_path):
+        return send_from_directory(os.path.dirname(__file__), path)
+    else:
+        abort(404, description="File not found")
 
-            with open(file_path, 'rb') as f:
-                response = f.read()
-            self.wfile.write(response)
-        elif self.path == "/get-molecules":
-            db = Database(reset=False)
-            db.create_tables()
-            molecules = db.conn.execute("SELECT * FROM Molecules")
+@app.route('/get-molecules', methods=['GET'])
+def get_molecules():
+    db = Database(reset=False)
+    db.create_tables()
+    molecules = db.conn.execute("SELECT * FROM Molecules")
 
-            molecule_dicts = []
-            for molecule in molecules:
-                molecule_dict = {
-                    "NAME": molecule[1] 
-                }
-                molecule_dicts.append(molecule_dict)
+    molecule_dicts = []
+    for molecule in molecules:
+        molecule_dict = {
+            "NAME": molecule[1]
+        }
+        molecule_dicts.append(molecule_dict)
 
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(bytes(json.dumps(molecule_dicts), "utf-8"))
-        else:
-            self.send_response(404)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+    return jsonify(molecule_dicts)
 
-            response = "<html><body><h1>File not found</h1></body></html>"
-            self.wfile.write(response.encode())
+@app.route('/upload-sdf', methods=['POST'])
+def upload_sdf():
+    if 'sdf_file' not in request.files:
+        abort(400, description="No file provided")
 
-    def do_POST(self):
-        if self.path == '/upload-sdf':
-            content_type, pdict = cgi.parse_header(self.headers.get("Content-Type"))
-            
-            if content_type == "multipart/form-data":
-                pdict["boundary"] = bytes(pdict["boundary"], "utf-8")
-                pdict["CONTENT-LENGTH"] = int(self.headers.get("Content-Length"))
-                form_data = cgi.parse_multipart(self.rfile, pdict)
+    file_data = request.files['sdf_file']
 
-                file_data = form_data["sdf_file"][0]
+    with open("temp.sdf", "wb") as f:
+        f.write(file_data.read())
 
-                with open("temp.sdf", "wb") as f:
-                    f.write(file_data)
+    with open("temp.sdf", "r") as f:
+        found = False
+        name = ""
+        for line in f:
+            if found:
+                name = line.strip()
+                break
+            if "<NAME>" in line:
+                found = True
+    fp = open("temp.sdf", "r")
 
-                with open("temp.sdf", "r") as f:
-                    found = False
-                    name = ""
-                    for line in f:
-                        if found:
-                            name = line.strip()
-                            break
-                        if "<NAME>" in line:
-                            found = True
-                fp = open("temp.sdf", "r")
+    db = Database(reset=False)
+    db.create_tables()
+    db.add_molecule(name, fp)
 
-                db = Database(reset=False)
-                db.create_tables()
-                db.add_molecule(name, fp)
+    os.remove("temp.sdf")
+    return "File uploaded successfully."
 
-                self.send_response(200)
-                self.send_header("Content-Type", "text/plain")
-                self.end_headers()
-                self.wfile.write(b"File uploaded successfully.")
+@app.route('/get-svg', methods=['POST'])
+def get_svg():
+    data = request.get_json()
 
-                os.remove("temp.sdf")
-        elif self.path == '/get-svg':
-            # Read the request data
-            content_length = int(self.headers["Content-Length"])
-            post_data = self.rfile.read(content_length)
+    if not data or "name" not in data:
+        abort(400, description="Molecule name not provided")
 
-            # Parse the JSON data
-            data = json.loads(post_data)
+    molecule_name = data["name"]
 
-            # Access the molecule name
-            molecule_name = data["name"]
+    db = Database(reset=False)
+    mol_display.RADIUS = db.radius()
+    mol_display.ELEMENT_NAME = db.element_name()
+    mol_display.HEADER += db.radial_gradients()
+    mol = db.load_mol(molecule_name)
+    mol.sort()
+    svg_filename = molecule_name + ".svg"
+    with open(svg_filename, "w") as fp:
+        fp.write(mol.svg())
 
-            # Process the molecule name as needed (e.g., retrieve the SVG)
-            db = Database(reset=False)
-            mol_display.RADIUS = db.radius()
-            mol_display.ELEMENT_NAME = db.element_name()
-            mol_display.HEADER += db.radial_gradients()
-            mol = db.load_mol(molecule_name)
-            mol.sort()
-            svg_filename = molecule_name + ".svg"
-            with open(svg_filename, "w") as fp:
-                fp.write(mol.svg())
+    with open(svg_filename, "r") as fp:
+        svg_content = fp.read()
 
-            # Read the SVG content from the file
-            with open(svg_filename, "r") as fp:
-                svg_content = fp.read()
+    os.remove(svg_filename)
+    return svg_content, 200, {"Content-Type": "image/svg+xml"}
 
-            # Send the response with the SVG content
-            self.send_response(200)
-            self.send_header("Content-Type", "image/svg+xml")
-            self.end_headers()
-            self.wfile.write(svg_content.encode())
-
-            # Remove the temporary SVG file
-            os.remove(svg_filename)
-
-
-PORT = 8000
-handler = MyRequestHandler
-
-with socketserver.TCPServer(("", PORT), handler) as httpd:
-    httpd.serve_forever()
+if __name__ == '__main__':
+    app.run()
