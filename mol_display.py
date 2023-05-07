50823e0296b8257eee481dae84e56b8c2f1e59aa
Ankush Madharha <ankush@Ankushs-MacBook-Pro.local>
2023-05-07 14:26:12 -0400

    added function to adjust viewbox

--- mol_display.py
+++ mol_display.py
@@ -1,10 +1,12 @@
 from molecule import molecule
+import re
+from xml.etree import ElementTree
 
 # CONSTANTS
 RADIUS = None
 ELEMENT_NAME = None
 
-HEADER = """<svg version="1.1" width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">"""
+HEADER = """<svg version="1.1" width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">\n"""
  
 FOOTER = """</svg>"""
 
@@ -38,7 +40,7 @@ class Atom:
         radi = RADIUS[self.atom.element]
         color = ELEMENT_NAME[self.atom.element]
 
-        return f'  <circle cx="{x}" cy="{y}" r="{radi}" fill="url(#{color})"/>\n  <text x="{x-10}" y="{y+10}" font-size="24" font-family="Arial" fill="lightgrey">{self.atom.element}</text>'
+        return f'\t<circle cx="{x}" cy="{y}" r="{radi}" fill="url(#{color})"/>\n\t<text x="{x-10}" y="{y+10}" font-size="24" font-family="Arial" fill="lightgrey">{self.atom.element}</text>\n'
     
 class Bond:
     def __init__(self, c_bond):
@@ -74,7 +76,7 @@ class Bond:
         x4 = ((self.bond.x2 * 100) + OFFSET_X) - (self.bond.dy * 10)
         y4 = ((self.bond.y2 * 100) + OFFSET_Y) + (self.bond.dx * 10)
 
-        return '  <polygon points="%.2f,%.2f %.2f,%.2f %.2f,%.2f %.2f,%.2f" fill="green"/>\n' % (x1, y1, x2, y2, x3, y3, x4, y4)
+        return '\t<polygon points="%.2f,%.2f %.2f,%.2f %.2f,%.2f %.2f,%.2f" fill="green"/>\n' % (x1, y1, x2, y2, x3, y3, x4, y4)
 
 class Molecule(molecule):
     def svg(self) -> str:
@@ -136,3 +138,54 @@ class Molecule(molecule):
             line = file.readline().strip()
             bond_info = line.split()
             self.append_bond(int(bond_info[0])-1, int(bond_info[1])-1, int(bond_info[2]))
+
+    def adjust_svg_viewbox(self, svg_file: str) -> str:
+        """
+        Adjust the viewbox of an SVG file to fit all circle elements, with an additional 200x200 padding.
+
+        Args:
+            svg_file (str): Path to the input SVG file.
+
+        Returns:
+            str: The updated SVG content as a string.
+        """
+        with open(svg_file, 'r') as file:
+            svg_content = file.read()
+
+        root = ElementTree.fromstring(svg_content)
+        circles = root.findall(".//{http://www.w3.org/2000/svg}circle")
+        coords = [(float(circle.get('cx')), float(circle.get('cy'))) for circle in circles]
+
+        min_x = min(x for x, y in coords) - 100
+        min_y = min(y for x, y in coords) - 100
+        max_x = max(x for x, y in coords) + 100
+        max_y = max(y for x, y in coords) + 100
+
+        viewBox = f"{min_x} {min_y} {max_x - min_x} {max_y - min_y}"
+        root.set('viewBox', viewBox)
+
+        updated_svg_content = ElementTree.tostring(root, encoding='unicode')
+
+        return updated_svg_content
+
+    
+    def remove_namespace_prefix(self, svg_content: str, prefix: str = "ns0:") -> str:
+        """
+        Removes a specified namespace prefix from an SVG content string.
+
+        Args:
+            svg_content (str): The SVG content string to process.
+            prefix (str, optional): The namespace prefix to remove. Defaults to "ns0:".
+
+        Returns:
+            str: The SVG content string with the specified namespace prefix removed.
+        """
+        pattern_open = re.compile(rf"<{prefix}(\w+)")
+        svg_content = pattern_open.sub(r"<\1", svg_content)
+
+        pattern_close = re.compile(rf"</{prefix}(\w+)>")
+        svg_content = pattern_close.sub(r"</\1>", svg_content)
+
+        return svg_content
+
+
